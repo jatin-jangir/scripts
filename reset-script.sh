df74b4596c86c5955e0e8fabd0750e220e187c7c
jatin-jangir <79351203+jatin-jangir@users.noreply.github.com>
2024-04-15 16:08:13 +0530

    Update reset-script.sh

--- reset-script.sh
+++ reset-script.sh
@@ -2,6 +2,16 @@
 echo "testing connection"
 PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "\l"
 
+echo "LENS_DB-----"
+echo $LENS_DB
+echo "GIT_SENSOR_DB-----"
+echo $GIT_SENSOR_DB
+echo "CASBIN_DB-----"
+echo $CASBIN_DB
+echo "ORCHESTRATOR_DB-----"
+echo $ORCHESTRATOR_DB
+
+
 echo "deleting and creating new DB"
 PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "DROP DATABASE $LENS_DB (FORCE);"
 PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "CREATE DATABASE $LENS_DB ;"
@@ -30,14 +40,15 @@ git clone https://github.com/devtron-labs/lens -b $LENS_BRANCH
 
 
 migrate -path ./devtron/scripts/sql -database postgres://$DB_CRED$DB_HOST:$DB_PORT/$ORCHESTRATOR_DB?sslmode=disable up;
-
+PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $ORCHESTRATOR_DB -t -c "select * from schema_migration;"
 migrate -path ./devtron/scripts/casbin  -database postgres://$DB_CRED$DB_HOST:$DB_PORT/$CASBIN_DB?sslmode=disable up;
-
+PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $CASBIN_DB -t -c "select * from schema_migration;"
 
 migrate -path ./git-sensor/scripts/sql  -database postgres://$DB_CRED$DB_HOST:$DB_PORT/$GIT_SENSOR_DB?sslmode=disable up;
-
+PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $GIT_SENSOR_DB -t -c "select * from schema_migration;"
 
 migrate -path ./lens/scripts/sql  -database postgres://$DB_CRED$DB_HOST:$DB_PORT/$LENS_DB?sslmode=disable up;
+PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $LENS_DB -t -c "select * from schema_migration;"
 
 echo "done migration"
 
